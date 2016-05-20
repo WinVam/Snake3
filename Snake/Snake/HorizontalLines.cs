1526acb46ee62790e1c916640353e2748c870aba
WinVam <job-smallsun777@yandex.ru>
2016-05-20 12:24:48 +0300

    добавлен класс HorizontalLines

---
+++ Snake/Snake/HorizontalLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLines
+    {
+        List<Point> pList;
+
+        public HorizontalLines(int xLeft, int xRigth, int y, char symb)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRigth; x++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,37 +16,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(5, 6, '*');
-            p3.Draw();
-
-            Point p4 = new Point(6, 7, '*');
-
-            /*List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);*/
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-            foreach(Point i in pList)
-            {
-                i.Draw();
-            }
+            HorizontalLines line = new HorizontalLines(1, 10, 6, '^');
+            line.Drow();
 
             Console.ReadLine();
         }
