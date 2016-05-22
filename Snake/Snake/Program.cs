50206baeac4d06f2264eadd32c345b9c3ae918a4
WinVam <job-smallsun777@yandex.ru>
2016-05-22 15:37:11 +0300

    управляемая змейка

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -29,12 +29,16 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGTH);
             snake.Drow();
 
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
 
             Console.ReadLine();
         }
